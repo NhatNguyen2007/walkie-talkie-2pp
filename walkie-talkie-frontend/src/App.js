9b5308212364807be83f856472e03b5ca4b87d1b
NhatNguyen2007 <nguyenminhnhatip@gmail.com>
2023-02-11 11:42:13 +0700

    first

--- walkie-talkie-frontend/src/App.js
+++ walkie-talkie-frontend/src/App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import './App.css';
 
 const { Device } = require('twilio-client');
 
@@ -12,6 +12,11 @@ class App extends Component {
       status: '',
       ready: false
     }
+
+    this.onChangeUpdateState = this.onChangeUpdateState.bind(this);
+    this.setup = this.setup.bind(this);
+    this.connect = this.connect.bind(this);
+    this.disconnect = this.disconnect.bind(this);
   }
 
   componentDidMount() {
@@ -51,9 +56,36 @@ class App extends Component {
   }
 
 
+  connect() {
+    const recipient = this.state.identity === 'friend1' ? 'friend2' : 'friend1';
+    this.state.device.connect({recipient: recipient});
+  }
+
+  disconnect() {
+    this.state.device.disconnectAll()
+  }
+
+
   setup(event) {
     // prevents form submission and page refresh
     event.preventDefault();
+
+    fetch(`https://walkie-talkie-service-3394-dev.twil.io/token?identity=${this.state.identity}`)
+    .then(response => response.json())
+    .then(data => {
+      this.state.device.setup(data.accessToken);
+      this.state.device.audio.incoming(false);
+      this.state.device.audio.outgoing(false);
+      this.state.device.audio.disconnect(false);
+    })
+    .catch(err => console.log(err))
+  }
+
+
+  onChangeUpdateState(event) {
+    this.setState({
+      identity: event.target.value
+    });
   }
 
 
@@ -62,13 +94,17 @@ class App extends Component {
       <div className="App">
         { 
           this.state.ready
-          ? <button>
+          ? <button
+              onMouseDown={this.connect}
+              onMouseUp={this.disconnect}>
               Press to Talk
             </button> 
           : <div>
               <p>Enter your name to begin.</p>
-              <form>
+              <form onSubmit={this.setup}>
                 <input 
+                  value={this.state.identity}
+                  onChange={this.onChangeUpdateState}
                   type="text" 
                   placeholder="What's your name?"></input>
                 <input type="submit" value="Begin Session"></input>
